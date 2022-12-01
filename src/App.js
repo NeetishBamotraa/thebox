9dc970c86af8f82a1710475863cc27768e81f65d
NeetishBamotraa <66291255+NeetishBamotraa@users.noreply.github.com>
2022-12-01 20:23:06 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
-  return <div className="mainbg">
-    <div className='main-style-bar-up'></div>
-    <div className='main-style-bar-down'></div>
-    <div className='center-box'></div>
-  </div>;
+  const [opt1, setopt1] = useState('d');
+
+  return (
+    <div className="mainbg">
+      <div className="main-style-bar-up">SELECT</div>
+      <div className="main-style-bar-down">Let's Play..</div>
+      {opt1 === 'd' && (
+        <div className="center-box">
+          <div onClick={() => setopt1('a')} className="first-options-bar1">
+            <span>Fibonacci Series</span>
+          </div>
+          <div onClick={() => setopt1('b')} className="first-options-bar1">
+            <span>Arithmetic Series</span>
+          </div>
+          <div onClick={() => setopt1('c')} className="first-options-bar1">
+            <span>Geometric Series</span>
+          </div>
+        </div>
+      )}
+      {opt1 === 'a' && <div className="center-box"></div>}
+    </div>
+  );
 }
